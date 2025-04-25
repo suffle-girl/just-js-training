fe85a735d84be880ad117fd069fb4f198e2d09d4
Šárka Flídrová <sarka.flidrova@email.cz>
2025-04-25 17:19:15 +0200

    romanToDecimal

--- index.js
+++ index.js
@@ -266,3 +266,43 @@ const breakCamelCase = (string) => {
 };
 console.log("breakCamelCase: ", breakCamelCase("camelCasingIsCool"));
 // add (?=...) to the regex in order to not discard the chars we are looking for
+
+// Turn Roman numeral as  argument into a numeric decimal integer without validating the form of the Roman numeral.
+
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// Example:
+// "MM"      -> 2000
+// "MDCLXVI" -> 1666
+// "M"       -> 1000
+// "CD"      ->  400
+// "XC"      ->   90
+// "XL"      ->   40
+// "I"       ->    1
+
+const romanToDecimal = (str) => {
+  let year = 0;
+
+  const values = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1,
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    const currentValue = values[str[i]];
+    const nextValue = values[str[i + 1]];
+
+    if (nextValue > currentValue) {
+      year -= currentValue;
+    } else {
+      year += currentValue;
+    }
+  }
+  return year;
+};
+console.log("romanToDecimal: ", romanToDecimal("MDCLXVI"));
