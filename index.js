3de84664536fe0612f1ffdde2ab1e2889d6efa57
Šárka Flídrová <sarka.flidrova@email.cz>
2025-06-10 16:05:40 +0200

    who likes

--- index.js
+++ index.js
@@ -524,3 +524,26 @@ const sumOddNumbers = (n) => {
 const removeExclamationMarks = (str) => {
   return str.replace(/\!/g, "");
 };
+
+// Show who likes the post - updated function with a switch statement
+const likes = (names) => {
+  names = names || [];
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    //break;
+    case 1:
+      return `${names[0]} likes this`;
+      break;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      break;
+    default:
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
+  }
+};
